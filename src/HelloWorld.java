e9384b57c65147361e2b5fdee85412a2210f7a01
Patrick <pseminatore@gmail.com>
2019-08-29 11:39:58 -0700

    add .gitignore

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main (String[] args){
-        System.out.println("My second hello world!");
+        System.out.println("My second hello world! heyyyyyyyy");
     }
 }
